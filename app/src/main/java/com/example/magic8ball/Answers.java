61c0b7ffeeb74528d6b06d8aff8c5d46ce915fe2
alanoli99 <alanoli99@gmail.com>
2023-04-26 11:29:31 -0400

    edited the questions into an arrayList

--- app/src/main/java/com/example/magic8ball/Answers.java
+++ app/src/main/java/com/example/magic8ball/Answers.java
@@ -1,29 +1,37 @@
 package com.example.magic8ball;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Answers {
-    /**
-     * ● It is certain.
-     * ● It is decidedly so.
-     * ● Without a doubt.
-     * ● Yes definitely.
-     * ● You may rely on it.
-     *
-     * ● As I see it, yes.
-     * ● Most likely.
-     * ● Outlook good.
-     * ● Yes.
-     * ● Signs point to yes.
-     *
-     * ● Reply hazy, try again.
-     * ● Ask again later.
-     * ● Better not tell you now.
-     * ● Cannot predict now.
-     * ● Concentrate and ask again.
-     *
-     * ● Don't count on it.
-     * ● My reply is no.
-     * ● My sources say no.
-     * ● Outlook not so good.
-     * ● Very doubtful.
-     */
+    private static final List<String> responses = Arrays.asList(
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+    );
+
+    private static final Random random = new Random();
+
+    public static String getRandomAnswer() {
+        int index = random.nextInt(responses.size());
+        return responses.get(index);
+    }
 }
